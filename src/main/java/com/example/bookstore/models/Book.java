c27ae9d9da9d8f69029902f43faa68725cb9344a
EvgeniySherg <Shergunov.evgeniy@gmail.com>
2025-03-30 14:51:33 +0300

    part 2: БД создается, запросы к ней обрабатываются корректно. Начинаем работу с DTO

--- src/main/java/com/example/bookstore/models/Book.java
+++ src/main/java/com/example/bookstore/models/Book.java
@@ -5,10 +5,10 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
-
+/// Почему то Getter и Setter не отрабатывают. Если не писать их самому то нельзя получить доступ к полям
 @Entity
-@RequiredArgsConstructor
-@NoArgsConstructor// create  getters, setters, hashcode, toString and Constructor with args
+@NoArgsConstructor
+@Data// create  getters, setters, hashcode, toString and Constructor with args
 @Table(name = "book")
 public class Book {
 
@@ -37,64 +37,64 @@ public class Book {
     @Column(name = "cover", nullable = false)
     private Cover coverType;
 
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public @NotEmpty(message = "Название книги не может быть пустым") String getName() {
-        return name;
-    }
-
-    public void setName(@NotEmpty(message = "Название книги не может быть пустым") String name) {
-        this.name = name;
-    }
-
-    public @NotEmpty(message = "Имя автора не может быть пустым") String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(@NotEmpty(message = "Имя автора не может быть пустым") String author) {
-        this.author = author;
-    }
-
-    public @NotEmpty(message = "Наименование издателя не может быть пустым") String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(@NotEmpty(message = "Наименование издателя не может быть пустым") String publisher) {
-        this.publisher = publisher;
-    }
-
-    public @NotNull(message = "колличество книг не может быть null") Integer getCount() {
-        return count;
-    }
-
-    public void setCount(@NotNull(message = "колличество книг не может быть null") Integer count) {
-        this.count = count;
-    }
-
-    public Cover getCoverType() {
-        return coverType;
-    }
-
-    public void setCoverType(Cover coverType) {
-        this.coverType = coverType;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", author='" + author + '\'' +
-                ", publisher='" + publisher + '\'' +
-                ", count=" + count +
-                ", coverType=" + coverType +
-                '}';
-    }
+//
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+//
+//    public  String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public  String getAuthor() {
+//        return author;
+//    }
+//
+//    public void setAuthor(String author) {
+//        this.author = author;
+//    }
+//
+//    public String getPublisher() {
+//        return publisher;
+//    }
+//
+//    public void setPublisher( String publisher) {
+//        this.publisher = publisher;
+//    }
+//
+//    public Integer getCount() {
+//        return count;
+//    }
+//
+//    public void setCount(Integer count) {
+//        this.count = count;
+//    }
+//
+//    public Cover getCoverType() {
+//        return coverType;
+//    }
+//
+//    public void setCoverType(Cover coverType) {
+//        this.coverType = coverType;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Book{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", author='" + author + '\'' +
+//                ", publisher='" + publisher + '\'' +
+//                ", count=" + count +
+//                ", coverType=" + coverType +
+//                '}';
+//    }
 }
---
+++ src/main/java/com/example/bookstore/models/BookDto.java
@@ -0,0 +1,15 @@
+package com.example.bookstore.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/// В BookDto мы определяем какие данные отдавать клиенту
+/// в данном случае избавились от id, count, а также изменили поле name на title
+@NoArgsConstructor
+@Data
+public class BookDto {
+    private String title;
+    private String author;
+    private String publisher;
+    private String coverType;
+}
