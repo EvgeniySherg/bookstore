2042f718f56709c0f8d51586a88731ad43749121
EvgeniySherg <Shergunov.evgeniy@gmail.com>
2025-04-13 14:55:34 +0300

    part 3.1: сделали наброски DTO

--- src/main/java/com/example/bookstore/mappers/BookMapper.java
+++ src/main/java/com/example/bookstore/mappers/BookMapper.java
@@ -3,9 +3,14 @@ package com.example.bookstore.mappers;
 import com.example.bookstore.dto.BookDto;
 import com.example.bookstore.models.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper (componentModel = "spring")
 public interface BookMapper {
-    Book toBook(BookDto bookDto);
-    BookDto toDto(Book book);
+
+    @Mapping(source = "name", target = "title")
+    Book toDto(Book book);
+
+    @Mapping(source = "title", target = "name")
+    BookDto toEntity(BookDto bookDto);
 }
